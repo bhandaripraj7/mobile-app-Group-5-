a3fd7ea0c0a1eb89483686689822a86121835f9c
bhandaripraj7 <112625606+bhandaripraj7@users.noreply.github.com>
2024-03-26 21:00:05 +0200

    Add files via upload
    Created mobile application project

---
+++ Mobiledevelopmentproject/app/src/main/java/com/example/mobiledevelopmentproject/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.mobiledevelopmentproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobiledevelopmentproject.ui.theme.MobileDevelopmentProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MobileDevelopmentProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MobileDevelopmentProjectTheme {
+        Greeting("Android")
+    }
+}
